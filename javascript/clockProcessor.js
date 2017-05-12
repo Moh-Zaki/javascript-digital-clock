c0092ae5e0ffeb10cefb813a98a8d34d64750cb8
dkluin <dcluynserf@gmail.com>
2017-05-12 11:45:09 +0200

    added alarm function + several bug fixes

--- javascript/clockProcessor.js
+++ javascript/clockProcessor.js
@@ -9,9 +9,17 @@ var ms_bShouldCountStopwatch = false;
 var ms_bClockInLoadingStage = true;
 var m_bInitialLoadingModePassed = false;
 
+var ms_bInitializedAlarm = false;
+var ms_bAlarmSet = false;
+var ms_bAlarmGoingOff = false;
+var ms_bAlarmOn = false;
+var ms_nTargetTime;
+
 function ProcessClock()
 {
 	var m_szTimeString = "";
+	var m_bShouldBeAM = false;
+	var m_bShouldBeAlarmAM = false;
 	m_nClockFramesPassed++;
 	var m_CurrDate = new Date();
 
@@ -21,9 +29,58 @@ function ProcessClock()
 		m_bInitialLoadingModePassed = true;
 	}
 
+	m_szVSHour = "";
+	m_szVSAlarmHour = "";
+	if (m_CurrDate.getHours() < 12)
+	{
+		m_bShouldBeAM = true;
+	}
+	else
+	{
+		m_bShouldBeAM = false;
+	}
+	if (m_CurrDate.getHours() != 12)
+	{
+		m_szVSHour = FormatClockNumber(m_CurrDate.getHours() % 12);
+	}
+	else
+	{
+		m_szVSHour = FormatClockNumber(m_CurrDate.getHours());
+	}
+
+	// Alarm 
+	if (ms_bAlarmSet)
+	{
+		if (ms_nTargetTime.getHours() < 12)
+		{
+			m_bShouldBeAlarmAM = true;
+		}
+		else
+		{
+			m_bShouldBeAlarmAM = false;
+		}
+		if (ms_nTargetTime.getHours() != 12)
+		{
+			m_szVSAlarmHour = FormatClockNumber(ms_nTargetTime.getHours() % 12);
+		}
+		else
+		{
+			m_szVSAlarmHour = FormatClockNumber(ms_nTargetTime.getHours());
+		}
+	}
+
 	// Standaard klok - met of zonder stopwatch
 	if ((m_nClockFramesPassed % 100) == 99)
 	{
+		// Alarm going off
+		if (ms_bAlarmOn && ms_bAlarmSet)
+		{
+			if (ms_nTargetTime.getHours() == m_CurrDate.getHours() && ms_nTargetTime.getMinutes() == m_CurrDate.getMinutes())
+			{
+				ms_bAlarmGoingOff = true;
+			}
+		}
+
 		if (ms_bClockInLoadingStage)
 		{
 			SwitchToLoadingMode(false);
@@ -34,7 +91,9 @@ function ProcessClock()
 			case 'CLOCKMODE_NL':
 			m_szTimeString = "";
 			document.querySelector("#clock-time-text-date").innerHTML = getDayName(m_CurrDate.getDay()) + " " + FormatClockNumber(m_CurrDate.getDate()) + " " + getMonthName(m_CurrDate.getMonth()) + " " + " " + m_CurrDate.getFullYear();
+			
 			m_szTimeString += FormatClockNumber(m_CurrDate.getHours()) + ":" + FormatClockNumber(m_CurrDate.getMinutes()) + ":" + FormatClockNumber(m_CurrDate.getSeconds());
+
 			document.querySelector("#clock-time-text").innerHTML = m_szTimeString;
 			if (m_CurrDate.getHours() > 19 || m_CurrDate.getHours() < 6)
 			{
@@ -51,24 +110,6 @@ function ProcessClock()
 			break;
 			case 'CLOCKMODE_VS':
 			m_szTimeString = "";
-			m_szVSHour = "";
-			var m_bShouldBeAM;
-			if (m_CurrDate.getHours() < 12)
-			{
-				m_bShouldBeAM = true;
-			}
-			else
-			{
-				m_bShouldBeAM = false;
-			}
-			if (m_CurrDate.getHours() != 12)
-			{
-				m_szVSHour = FormatClockNumber(m_CurrDate.getHours() % 12);
-			}
-			else
-			{
-				m_szVSHour = FormatClockNumber(m_CurrDate.getHours());
-			}
 
 			document.querySelector("#clock-time-text-date").innerHTML = getDayName(m_CurrDate.getDay()) + " " + FormatClockNumber(m_CurrDate.getDate()) + " " + getMonthName(m_CurrDate.getMonth()) + " " + " " + m_CurrDate.getFullYear();
 			if (m_bShouldBeAM)
@@ -118,6 +159,50 @@ function ProcessClock()
 			document.querySelector("#clock-stopwatch-time").style.textShadow = "none";			
 		}
 	}
+
+	// Alarm
+	if (ms_bAlarmSet)
+	{
+		if (ms_bInitializedAlarm)
+		{
+			if (m_szClockMode == "CLOCKMODE_NL")
+			{
+				document.querySelector("#clock-time-alarm-text").innerHTML = "<small>ALARM SET - " + FormatClockNumber(ms_nTargetTime.getHours()) + ":" + FormatClockNumber(ms_nTargetTime.getMinutes()) + "</small>";
+			}
+			else
+			{
+				if (m_bShouldBeAlarmAM)
+				{
+					document.querySelector("#clock-time-alarm-text").innerHTML = "<small>ALARM SET - " + m_szVSAlarmHour + ":" + FormatClockNumber(ms_nTargetTime.getMinutes()) + " AM" + "</small>";
+				}
+				else
+				{
+					document.querySelector("#clock-time-alarm-text").innerHTML = "<small>ALARM SET - " + m_szVSAlarmHour + ":" + FormatClockNumber(ms_nTargetTime.getMinutes()) + " PM" + "</small>";
+				}
+			}
+	
+			if (m_CurrDate.getHours() > 19 || m_CurrDate.getHours() < 6)
+			{
+				document.querySelector("#clock-time-alarm-text").style.textShadow = "0 0 7.5px orange, 0 0 7.5px orange";
+			}
+			else
+			{
+				document.querySelector("#clock-time-alarm-text").style.textShadow = "none";	
+			}
+
+			if (ms_bAlarmGoingOff)
+			{
+				if ((m_nClockFramesPassed % 100) > 49)
+				{
+					document.querySelector("#clock-viewport").style.background = "white";
+				}
+				else
+				{
+					document.querySelector("#clock-viewport").style.background = "black";
+				}
+			}
+		}
+	}
 }
 
 function SwitchToLoadingMode(m_bNewLoadingMode)
@@ -131,6 +216,7 @@ function SwitchToLoadingMode(m_bNewLoadingMode)
 		document.querySelector("#clock-time-text-date").innerHTML = "Donderdag 1 Januari 1970";
 		document.querySelector("#clock-time-text").innerHTML = "00:00:00";
 		document.querySelector("#clock-stopwatch-time").style.textShadow = "0 0 7.5px orange, 0 0 7.5px orange";
+		document.querySelector("#clock-time-alarm-text").style.textShadow = "0 0 7.5px orange, 0 0 7.5px orange";
 		document.querySelector("#clock-stopwatch-time").innerHTML = "Laden...";
 		ms_bClockInLoadingStage = m_bNewLoadingMode;
 	}
@@ -285,4 +371,65 @@ function processStopwatchTime()
 	var m_StopwatchDate = new Date(m_nStopwatchTimer * 10);
 	m_szStopwatchString += FormatClockNumber(m_StopwatchDate.getUTCHours()) + ":" + FormatClockNumber(m_StopwatchDate.getMinutes()) + ":" + FormatClockNumber(m_StopwatchDate.getSeconds()) + " " + FormatMillisecondsNumber(m_StopwatchDate.getMilliseconds());
 	return m_szStopwatchString; 
+}
+
+function ToggleAlarm(m_szAlarmMode)
+{
+	switch (m_szAlarmMode)
+	{
+		case 'ALARM_ON':
+		ms_bAlarmSet = false;
+		ms_bAlarmGoingOff = false;
+		ms_bAlarmOn = true;
+		ms_nTargetTime = new Date();
+		ms_bInitializedAlarm = true;
+		document.querySelector("#alarm-btns").innerHTML = "";
+		document.querySelector("#alarm-btns").innerHTML += "<button onclick=\"ToggleAlarm('ALARM_OFF')\">Alarm uit</button>";
+		ToggleAlarmForm('ALARM_FORM_OPEN_WINDOW');
+		break;
+		case 'ALARM_OFF':
+		ms_bAlarmSet = false;
+		ms_bAlarmGoingOff = false;
+		ms_bAlarmOn = false;
+		ms_bInitializedAlarm = false;
+		document.querySelector("#clock-viewport").style.background = "black";
+		document.querySelector("#alarm-btns").innerHTML = "";
+		document.querySelector("#clock-time-alarm-text").innerHTML = "";
+		document.querySelector("#alarm-btns").innerHTML += "<button onclick=\"ToggleAlarm('ALARM_ON')\">Alarm aan</button>";
+		break;
+		// For debugging purposes only
+		case 'ALARM_TRIGGER_NOW':
+		if (ms_bAlarmOn && ms_bInitializedAlarm && !ms_bAlarmGoingOff)
+		{
+			ms_bAlarmGoingOff = true;
+		}
+		break;
+	}
+}
+
+function ToggleAlarmForm(m_szAlarmFormMode)
+{
+	switch (m_szAlarmFormMode)
+	{
+		case 'ALARM_FORM_SEND':
+		var a = document.querySelector("#alarm-form-input").value;
+		var m_nTime = a.split(":");
+		for (var i = 0; i < m_nTime.length; i++)
+		{
+			if (i == 0)
+			{
+				ms_nTargetTime.setHours(Number(m_nTime[i]));
+			}
+			else
+			{
+				ms_nTargetTime.setMinutes(Number(m_nTime[i]));
+			}
+		}
+		document.querySelector("#alarm-form-inner-container").style.display = "none";
+		ms_bAlarmSet = true;
+		break;
+		case 'ALARM_FORM_OPEN_WINDOW':
+		document.querySelector("#alarm-form-inner-container").style.display = "block";
+		break;
+	}
 }
